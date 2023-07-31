86187e1ffa05b935d5a5c025deb769d9873882f5
JakeSweeney <jake.sweeney.03@gmail.com>
2023-07-31 14:27:52 +0100

    WIP; correct test scenario for day02 part 1

--- src/test/java/aoc/day02/Day02Test.java
+++ src/test/java/aoc/day02/Day02Test.java
@@ -3,6 +3,7 @@ package aoc.day02;
 import aoc.day01.Day01;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,13 +14,13 @@ public class Day02Test {
     @Test
     public void testPart1(){
         // Given
-        List<String> input = Collections.singletonList("test");
-
+        List<String> input = Arrays.asList(new String [] {"A Y", "B X", "C Z"});
+        System.out.println("Got here");
         // When
-        String result = new Day01().part1(input);
+        String result = new Day02().part1(input);
 
         // Then
-        assertEquals(input.get(0), result);
+        assertEquals("15", result);
     }
 
     @Test
@@ -28,7 +29,7 @@ public class Day02Test {
         List<String> input = Collections.singletonList("test");
 
         // When
-        String result = new Day01().part2(input);
+        String result = new Day02().part2(input);
 
         // Then
         assertEquals(input.get(0), result);
