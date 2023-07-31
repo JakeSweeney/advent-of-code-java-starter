7940b5024fa3971ae0e27a2aeb4d5ccc5ac0fd18
JakeSweeney <jake.sweeney.03@gmail.com>
2023-07-31 13:46:21 +0100

    Creates a place to work on Day 2 problem

---
+++ src/test/java/aoc/day02/Day02Test.java
@@ -0,0 +1,36 @@
+package aoc.day02;
+
+import aoc.day01.Day01;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day02Test {
+
+    @Test
+    public void testPart1(){
+        // Given
+        List<String> input = Collections.singletonList("test");
+
+        // When
+        String result = new Day01().part1(input);
+
+        // Then
+        assertEquals(input.get(0), result);
+    }
+
+    @Test
+    public void testPart2(){
+        // Given
+        List<String> input = Collections.singletonList("test");
+
+        // When
+        String result = new Day01().part2(input);
+
+        // Then
+        assertEquals(input.get(0), result);
+    }
+}
